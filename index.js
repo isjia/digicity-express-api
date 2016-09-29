9db55a3707ee5e03dd1ed18a4196631f2b6d831f
Peter Wang <happypeter1983@gmail.com>
2016-09-29 10:07:45 +0800

    connected

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-var express = require('express')
-var app = express()
-
-app.get('/:name', function (req, res) {
-
-  var username = req.params.name
-  var page = "<html>" +
-               "<body>" +
-                 "<h1>" +
-                 username + "的购物车" +
-                 "</h1>" +
-               "</body>" +
-              "</html>"
-  res.send(page)
-})
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/digicity-express-api')
 
 
-app.post('/:name', function (req, res) {
-  res.send("a POST request received" + req.params.name)
-})
 
-app.get('/about.html', function (req, res) {
-  var page = "<html>" +
-               "<body>" +
-                 "<h1>About.html</h1>" +
-               "</body>" +
-              "</html>"
-  res.send(page)
-})
+var db = mongoose.connection
 
-app.listen(3000, function(){
-  console.log('running on port 3000...plz visit http://localhost:3000')
-})
+// db.on('error', console.error.bind(console, 'connection error:'))
+
+db.once('open', function() {
+   console.log('success!')
+});
