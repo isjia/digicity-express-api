17c1cd916fb612a0e26ec333ec16b3ba996b7d6f
Peter Wang <happypeter1983@gmail.com>
2016-09-29 15:27:34 +0800

    remove junk

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose')
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/api')
 
 
@@ -16,18 +17,25 @@ db.once('open', function() {
 
   var user = mongoose.model('user', userSchema);
   // cat 是实际数据库中记录的名字
-  var peter = new user({ name: 'pppeter', password: '111', age: '33' });
-  // 成功构建一条数据记录
-  peter.save()
+  // var peter = new user({ name: 'pppeter', password: '111', age: '33' });
+  // // 成功构建一条数据记录
+  // peter.save()
 
 
-  peter.name = 'peterpeter'
-  peter.password = 'aaabbb'
-  peter.save()
-
-
-  user.find().exec(function(err, users) {
-    // 异步执行
-    console.log(users);
+  user.findById({_id: '57ecbcdf4d95146106254fe0'}, function(err, user) {
+    user.name = 'rrrrr'
+    user.save(function(err){
+      console.log('更新了！')
+    });
   });
+
+  console.log("我先出来了")
+
+
+
+
+  // user.find().exec(function(err, users) {
+  //   // 异步执行
+  //   console.log(users);
+  // });
 })
