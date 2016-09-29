daf160243e3570b404c5a390cf3c179fe6af6a35
Peter Wang <happypeter1983@gmail.com>
2016-09-29 11:18:43 +0800

    i

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ var db = mongoose.connection
 // db.on('error', console.error.bind(console, 'connection error:'))
 
 db.once('open', function() {
-  var personSchema = mongoose.Schema({
+  var catSchema = mongoose.Schema({
    name: String
   })
 
-  var person = mongoose.model('pppp', personSchema);
-  var peter = new person({ name: 'Peter' });
-  console.log(peter.name);
+  var cat = mongoose.model('cat', catSchema);
+  // cat 是实际数据库中记录的名字
+  var kitty = new cat({ name: 'HooKitty' });
+  console.log(kitty.name);
   // 成功构建一条数据记录
+  kitty.save()
 })
