a585d2136ea6833d2fa81ebf90fe8fa98882ee7c
Peter Wang <happypeter1983@gmail.com>
2016-09-30 16:59:55 +0800

    i

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var  app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/react-express-api');
+
+var db = mongoose.connection;
+db.on('error', console.log);
+  db.once('open', function() {
+    console.log('success!')
+  });
 
 app.get('/write', function(req, res) {
   var page = "<form method='post' action='/posts'>" +
@@ -17,7 +26,8 @@ app.get('/posts', function(req, res) {
   console.log('GET /posts')
 })
 app.post('/posts/', function(req, res) {
-  res.send('the post title is: ' + req.body.title)
+  //res.send('the post title is: ' + req.body.title)
+  res.redirect('/posts')
 })
 app.listen(3000, function() {
   console.log('running on port 3000')
