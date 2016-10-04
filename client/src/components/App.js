a9c2d025c2490270378c73aaeeb1e68d611c9740
Peter Wang <happypeter1983@gmail.com>
2016-10-04 11:50:37 +0800

    i

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import PostList from './PostList';
 
 export default class App extends Component {
   constructor() {
     super();
-    this.state={
-      posts: []
-    };
   }
-  componentWillMount() {
-    console.log('hello will axios');
-    //  Promise
-    axios.get('http://localhost:3000/posts').then(res => {
-      console.log('axios');
-      this.setState({
-        posts: res.data.posts
-      });
-      console.log(this.state.posts);
-    });
-  }
+
   render() {
-    let posts = this.state.posts.map( (item,i) => <p key={i}>{item.title}</p>)
     return(
       <div>
-        {posts}
+        <PostList />
       </div>
     );
   }
