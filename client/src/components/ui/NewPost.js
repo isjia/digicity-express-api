c636788cd4d47924c889d0116ff4c12952a7bb38
Peter Wang <happypeter1983@gmail.com>
2016-10-06 11:57:19 +0800

    add demo

--- client/src/components/ui/NewPost.js
+++ client/src/components/ui/NewPost.js
@@ -22,6 +22,20 @@ class NewPost extends Component {
   }
   newPost(data){
     axios.post('http://localhost:3000/posts', data)
+
+    // 
+    // POST /posts  according to REST api structrue
+    // 例如把一篇博客当做一个 resource ，资源
+    // GET /posts     列出所有博客
+    // PUT /posts/:post_id 更新一篇博客
+    // DELETE /posts/:post_id  删除一篇博客
+    // GET /posts/:post_id  打开一篇博客
+    //
+    // REST 架构有两大优势
+    //   1. 请求的格式符合正常用户思维
+    //   2. 最大程度的尊重了 http verb 设计者的原始意图
+
+
     .then( res => {
       console.log(res.data.message);
       this.context.router.push('/');
