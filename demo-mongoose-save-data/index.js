60e5638c0bfb471f18cad23572a143e7d54a1982
Peter Wang <happypeter1983@gmail.com>
2016-10-07 16:01:11 +0800

    i

--- demo-mongoose-save-data/index.js
+++ demo-mongoose-save-data/index.js
@@ -1,5 +1,16 @@
 var express = require('express');
 
+var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/digicity-express-api');
+
+var db = mongoose.connection;
+db.on('error', console.log);
+db.once('open', function() {
+  console.log(' success!')
+});
+
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema(
@@ -9,13 +20,17 @@ var PostSchema = new Schema(
   }
 );
 
-var app = express();
-var mongoose = require('mongoose');
+var Post = mongoose.model('Post', PostSchema);
 
 
 
 app.post('/posts', function(req, res){
-  console.log('hello');
+  console.log('run post.save()');
+  var post = new Post({title:"myTitle", content: "myConent"})
+  post.save(function(err){
+    if(err) return console.log(err);
+    console.log('saved');
+  });
 });
 
 app.listen(3000, function(){
