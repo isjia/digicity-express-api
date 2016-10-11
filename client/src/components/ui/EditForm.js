96a6624aee751f314fb40e7e865e3d96bcb00f67
Peter Wang <happypeter1983@gmail.com>
2016-10-11 14:41:14 +0800

    i

--- client/src/components/ui/EditForm.js
+++ client/src/components/ui/EditForm.js
@@ -4,14 +4,15 @@ export default class EditForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     console.log('submit');
+    console.log(this.refs.title.value);
   }
    render() {
      return(
        <div>
-         <form onSubmit={this.handleSubmit}>
+         <form onSubmit={this.handleSubmit.bind(this)}>
           <div>
             <label>title</label>
-            <input defaultValue={this.props.post.title} />
+            <input ref="title" defaultValue={this.props.post.title} />
           </div>
            <div>
              <label>content</label>
