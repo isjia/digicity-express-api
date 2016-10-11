8baa0e3ab5d69b6e17f6ca910a7aaa9066131643
Peter Wang <happypeter1983@gmail.com>
2016-10-11 16:33:59 +0800

    i

--- server/index.js
+++ server/index.js
@@ -20,36 +20,10 @@ db.once('open', function() {
   console.log('success!')
 });
 
+var routes = require('./routes');
 
-app.get('/', function(req, res) {
-  res.send("this is api server");
-})
+routes(app);
 
-app.get('/posts', function(req, res) {
-  Post.find().exec(function(err, posts) {
-    res.json({ posts: posts})
-  });
-})
-app.get('/post/:id', function(req, res) {
-  Post.findById({_id:req.params.id},function (err,doc) {
-    if (err) return res.send('出错了');
-    res.json({post: doc})
-  })
-})
-app.post('/posts', function(req, res) {
-  // res.send('the post title is: ' + req.body.title)
-  console.log(req.body);
-  var post = new Post({
-    title:req.body.title,
-    category:req.body.category,
-    content:req.body.content
-  });
-  post.save(function(err){
-    if(err) return console.log(err);
-    console.log('saved!');
-  })
-  res.json({message:"保存成功"})
-})
 app.listen(3000, function() {
   console.log('running on port 3000')
 })
