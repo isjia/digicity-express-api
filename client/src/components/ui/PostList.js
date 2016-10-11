2561b8048e17b4a12588505e0a865f86b97415b8
Peter Wang <happypeter1983@gmail.com>
2016-10-11 09:33:17 +0800

    i

--- client/src/components/ui/PostList.js
+++ client/src/components/ui/PostList.js
@@ -12,40 +12,50 @@ export default class PostList extends Component {
     };
   }
   getStyles() {
-    return {
-      content: {
-        position: 'relative',
-        width: '100%',
-        maxWidth: '600px',
-        margin: '20px auto',
-        backgroundColor: '#fff',
-        borderRadius: '5px',
-        padding: '16px',
-        boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px',
-        minHeight: '80px'
-      },
-      title: {
-        fontSize: '1.2em'
-      },
-      link:{
-        color:'#fff',
-        display:'block',
-        width:'5em',
-        height:'2em',
-        backgroundColor:'#00bcd4',
-        textDecoration:'none',
-        lineHeight:'2em',
-        borderRadius:'5px',
-        textAlign:'center',
-        margin:'10px auto'
-      },
-      a:{
-        position:'absolute',
-        right:'16px',
-        top:'20px'
-      }
-    }
-  }
+     return {
+       content: {
+         position: 'relative',
+         width: '100%',
+         height: '60px',
+         maxWidth: '600px',
+         margin: '20px auto',
+         backgroundColor: '#fff',
+         borderRadius: '5px',
+         padding: '16px',
+         boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px'
+       },
+       title: {
+         fontSize: '1.2em'
+       },
+       button: {
+         display: 'block',
+         margin: '30px auto',
+         width: '120px',
+         height: '36px',
+         lineHeight: '36px',
+         textAlign: 'center',
+         backgroundColor: '#ff4081',
+         fontSize: '1em',
+         color: '#fff',
+         textDecoration: 'none',
+         borderRadius: '20px'
+       },
+       actions: {
+         position: 'absolute',
+         bottom: '16px',
+         right: '16px'
+       },
+       link: {
+         display: 'inline-block',
+         fontSize: '.9em',
+         color: '#00bcd4',
+         opacity: '.8',
+         textDecoration: 'none',
+         paddingLeft: '10px',
+         paddingRight: '10px'
+       }
+     }
+   }
   componentWillMount() {
     //  Promise
     axios.get('http://localhost:3000/posts').then(res => {
@@ -62,17 +72,16 @@ export default class PostList extends Component {
       return (
         <div style={styles.content} key={post._id}>
           <div style={styles.title}>{post.title}</div>
-          <div style={styles.a}>
+          <div style={styles.actions}>
             <Link to={`/posts/${post._id}`} style={styles.link}>查看</Link>
             <Link to={`/posts/${post._id}/edit`} style={styles.link}>编辑</Link>
-
           </div>
         </div>
       )
     }, this.state.posts);
     return(
       <div>
-        <Link to="/write" style={styles.link}>写文章</Link>
+        <Link to="/write" style={styles.button}>写文章</Link>
         { postList }
       </div>
     );
